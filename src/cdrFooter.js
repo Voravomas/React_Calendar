b460667988ee93f201f06b8b29349a3c3307ed10
Mykyta Samovarov <44195829+Voravomas@users.noreply.github.com>
2020-05-05 21:31:45 +0300

    Added space to cdrFooter.js

--- src/cdrFooter.js
+++ src/cdrFooter.js
@@ -3,7 +3,7 @@ import React from 'react';
 const CalendarFooter = ({description}) => (
     <div className="calendar-footer">
         <h2 className="ft-h2">{description.taskName}</h2>
-        <p className="ft-p">{description.taskMore}</p>
+        <p className="ft-p">{description.taskMore}</p> 
     </div>
 );
 
